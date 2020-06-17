838e0b08c5f07302cda42ee9ade9ce36feb421b9
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2020-06-17 16:58:58 -0400

    Update install_auto_connect.sh
    Add auth support

--- tutorials/QMI_tutorial/install_auto_connect.sh
+++ tutorials/QMI_tutorial/install_auto_connect.sh
@@ -3,10 +3,30 @@
 echo "What is the APN?"
 read carrierapn
 
+echo "What is the username (Leave empty if no auth is required)"
+read username
+
+if [ ! -z "$username" ]
+then
+    echo "What is the password"
+    read password
+
+    if [ -z "$password" ]
+    then
+        echo "Must provide a password"
+        exit 1
+    fi
+else
+    sed -i -e "s/ #USER//" qmi_reconnect.sh
+    sed -i -e "s/ #PASS//" qmi_reconnect.sh
+fi
+
 wget --no-check-certificate https://raw.githubusercontent.com/sixfab/Sixfab_RPi_3G-4G-LTE_Base_Shield/master/tutorials/QMI_tutorial/reconnect_service -O qmi_reconnect.service
 wget --no-check-certificate https://raw.githubusercontent.com/sixfab/Sixfab_RPi_3G-4G-LTE_Base_Shield/master/tutorials/QMI_tutorial/reconnect_sh -O qmi_reconnect.sh
 
 sed -i "s/#APN/$carrierapn/" qmi_reconnect.sh
+sed -i "s/#USER/$username/" qmi_reconnect.sh
+sed -i "s/#PASS/$password/" qmi_reconnect.sh
 
 mv qmi_reconnect.sh /usr/src/
 mv qmi_reconnect.service /etc/systemd/system/
